e578a517b5d6208b9111b2af1a10e1ff6853ceb9
Jamie Pace <jamiejpace@gmail.com>
2021-11-06 12:37:54 -0600

    Refactor food search endpoint and add tests, add vcr to specs

--- spec/requests/api/v1/food_search/index_spec.rb
+++ spec/requests/api/v1/food_search/index_spec.rb
@@ -1,9 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe "food search index endpoint" do
-  it 'returns foods from search' do
+RSpec.describe "food search index endpoint", :vcr do
+  it 'returns foods from search if a food search param is given' do
     get '/api/v1/foods/search', params: { food_search: "blueberry" }
 
     expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    foods = JSON.parse(response.body, symbolize_names: true)
+
+    expect(foods).to have_key(:data)
+    expect(foods[:data]).to be_an(Array)
+
+    foods[:data].each do |food|
+      expect(food).to have_key(:id)
+      expect(food[:id]).to be_a(String)
+
+      expect(food).to have_key(:type)
+      expect(food[:type]).to be_a(String)
+
+      expect(food).to have_key(:attributes)
+      expect(food[:attributes]).to be_a(Hash)
+
+      expect(food[:attributes]).to have_key(:name)
+      expect(food[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  it 'returns a 400 error if no search param is given' do
+    get '/api/v1/foods/search'
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
   end
 end
--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -1,7 +1,15 @@
-# require 'rails_helper'
-#
-# RSpec.describe FoodService do
-#   it 'finds food based on food name search' do
-#
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'food service', :vcr do
+  it 'finds food based on food name search' do
+    response = FoodService.food_search("frosted flakes")
+
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:hints)
+    expect(response[:hints]).to be_an(Array)
+    expect(response[:hints].first).to be_a(Hash)
+    expect(response[:hints].first[:food]).to be_a(Hash)
+    expect(response[:parsed].first[:food][:foodId]).to be_a(String)
+    expect(response[:parsed].first[:food][:label]).to be_a(String)
+  end
+end
