b65e1722f74e92f6f02c6d4bcaee1cbe4e16a4dd
Jamie Pace <jamiejpace@gmail.com>
2021-11-06 22:08:27 -0600

    Add tests for serializer and sad path for food entries endpoint

--- app/controllers/api/v1/food_entries_controller.rb
+++ app/controllers/api/v1/food_entries_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::FoodEntriesController < ApplicationController
     user = User.find_by(id: params[:id])
     if user
       foods = FoodEntry.foods_with_rank(user)
+      require 'pry'; binding.pry
       render json: FoodEntrySerializer.new(foods), status: 200
     else
       render json: {error: "bad request"}, status: 400
--- spec/requests/api/v1/foods/index_spec.rb
+++ spec/requests/api/v1/foods/index_spec.rb
@@ -15,5 +15,26 @@ RSpec.describe "food index endpoint" do
     expect(response.status).to eq(200)
 
     foods = JSON.parse(response.body, symbolize_names: true)
+
+    expect(foods).to be_a(Hash)
+    expect(foods).to have_key(:data)
+    expect(foods[:data]).to be_an(Array)
+    expect(foods[:data].first).to have_key(:attributes)
+    expect(foods[:data].first[:attributes][:name]).to eq("pop tart")
+    expect(foods[:data].first[:attributes][:average_rank]).to eq("7.0")
+  end
+
+  it 'returns an error if user does not exist' do
+    user = User.create!(name: "Billy Bob", email: "billybob@gmail.com", access_token: "daigrjslei23590sdgjkl", id: 1)
+    meal1 = Meal.create!(name: "First meal", user_id: user.id, rank: 6, meal_time: "00:00:00")
+    meal2 = Meal.create!(name: "Second meal", user_id: user.id, rank: 8, meal_time: "00:00:00")
+    meal3 = Meal.create!(name: "Third meal", user_id: user.id, rank: 5, meal_time: "00:00:00")
+    food_entry1 = FoodEntry.create!(meal_id: meal1.id, food_id: "abc", food_name: "pop tart")
+    food_entry2 = FoodEntry.create!(meal_id: meal2.id, food_id: "abc", food_name: "pop tart")
+
+    get "/api/v1/users/2/foods"
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
   end
 end
