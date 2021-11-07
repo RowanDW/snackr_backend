510981c7bcd3e7604e419bf801f9701438d47b6e
Jamie Pace <jamiejpace@gmail.com>
2021-11-07 12:04:19 -0700

    Add top 10 and bottom 10 methods for get food params

--- app/controllers/api/v1/food_entries_controller.rb
+++ app/controllers/api/v1/food_entries_controller.rb
@@ -2,12 +2,18 @@ class Api::V1::FoodEntriesController < ApplicationController
 
   def index
     user = User.find_by(id: params[:id])
-    if user
+    if user && params[:method] == "top_10"
+      foods = FoodEntry.ten_highest_ranked(user)
+      render json: FoodEntrySerializer.new(foods), status: 200
+    elsif user && params[:method] == "bottom_10"
+      foods = FoodEntry.ten_lowest_ranked(user)
+      render json: FoodEntrySerializer.new(foods), status: 200
+    elsif user && params[:method].nil?
       foods = FoodEntry.foods_with_rank(user)
-      require 'pry'; binding.pry
       render json: FoodEntrySerializer.new(foods), status: 200
     else
       render json: {error: "bad request"}, status: 400
     end
   end
+
 end
