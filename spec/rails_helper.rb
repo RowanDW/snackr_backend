cb2a5e4635e3888b2a135409c1bd76ec84adc23b
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-08 17:26:33 -0700

    Differentiate names of id and key

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'faraday'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -74,7 +73,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('Private API key') { ENV['app_id'] }
+  config.filter_sensitive_data('Private API id') { ENV['app_id'] }
   config.filter_sensitive_data('Private API key') { ENV['app_key'] }
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
