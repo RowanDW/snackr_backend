0197e4325a6eec1af9e199b7814c3f278facb322
sami-p <sami.peterson14@gmail.com>
2021-11-09 18:17:39 -0700

    Create graphs controller with index action

---
+++ app/controllers/api/v1/graphs_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::GraphsController < ApplicationController
+  def index
+    if User.exists?(params[:id])
+      user = User.find(params[:id])
+      highest = Graph.ten_highest_ranked(user)
+      lowest = Graph.ten_lowest_ranked(user)
+      render json: GraphSerializer.get_graphs(highest, lowest)
+    else
+      render json: {error: "not found"}, status: 404
+    end
+  end
+end
