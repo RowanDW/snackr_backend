baa51ccb1355ef4ebc1a683f7ceff5625ab9a88d
sami-p <sami.peterson14@gmail.com>
2021-11-09 18:17:17 -0700

    Rename graph route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       post '/users/:id/meals', to: 'meals#create'
       patch '/users/:id/meals', to: 'meals#update'
       get '/users/:id/foods', to: 'food_entries#index'
+      get '/users/:id/graphs', to: 'graphs#index'
     end
   end
 end
